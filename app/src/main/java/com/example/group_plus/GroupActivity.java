92962791a3cb7c047dd0941fb74c5b89aa1530f8
AW0723 <aweng@ucsd.edu>
2022-04-10 01:19:24 -0700

    Progress bar updates live with database, added some temporary test code

--- app/src/main/java/com/example/group_plus/GroupActivity.java
+++ app/src/main/java/com/example/group_plus/GroupActivity.java
@@ -1,6 +1,5 @@
 package com.example.group_plus;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.*;
@@ -10,9 +9,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,7 +19,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.util.JsonMapper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,6 +31,7 @@ public class GroupActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private FirebaseUser currUser;
     private DatabaseReference mProgressReference;
+    private int goal = 12;
     private HashMap<String, Integer> memberProg;
     //TODO add uuid for groups
     private int groupID = 1;
@@ -42,20 +41,46 @@ public class GroupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
 
+        Log.d(TAG, "group");
+
         // Initialize Firebase Auth and user
         mAuth = FirebaseAuth.getInstance();
         currUser = mAuth.getCurrentUser();
 
         // Initialize Firebase database
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        //TODO add group uuid
-        mProgressReference = mDatabase.child("Groups").child("1").child("Goals").child("Reading").child("Progress");
+        mProgressReference = mDatabase.child("progress");
 
         // Add listener for database change
         ValueEventListener progressListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Log.d(TAG, "Progress is " + dataSnapshot.getValue());
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(dataSnapshot.getValue().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.d(TAG, "Data: " + jsonObject);
+                Iterator<String> uidItr = jsonObject.keys();
+                int sumProgress = 0;
+                while(uidItr.hasNext()) {
+                    String uid = uidItr.next();
+                    try {
+                        sumProgress += jsonObject.getInt(uid);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                ProgressBar bookProgress = findViewById(R.id.book_progress_bar);
+                int progress = (int) ((float) sumProgress / (float) goal * 100);
+                bookProgress.setProgress(progress);
+
+                TextView progressText = findViewById(R.id.book_progress_text);
+                progressText.setText(sumProgress + "/" + goal + " Books");
+                Log.d(TAG, progressText.getText().toString());
             }
 
             @Override
@@ -67,61 +92,14 @@ public class GroupActivity extends AppCompatActivity {
         mProgressReference.addValueEventListener(progressListener);
 
         // Add onClickListener for save button
-        Button saveBtn = this.findViewById(R.id.save_button);
-        saveBtn.setOnClickListener(view -> {
+        Button addBtn = this.findViewById(R.id.add_button);
+        addBtn.setOnClickListener(view -> {
             EditText num1View = this.findViewById(R.id.books_read);
-//            EditText num2View = this.findViewById(R.id.number2);
 
             int num1Text = Integer.parseInt(num1View.getText().toString());
-            mDatabase.child("Users").child(currUser.getUid()).child(Integer.toString(groupID)).child("Reading").setValue(num1Text);
+            mDatabase.child("progress").child(currUser.getUid()).setValue(num1Text);
             Log.d(TAG, "Updated value in database: " + num1Text);
-            updateProgress();
         });
-    }
-
-    private void updateProgress() {
-        ArrayList<String> members = new ArrayList<>();
-
-        mDatabase.child("Groups").child("1").child("Members").get().addOnCompleteListener(task -> {
-            if (!task.isSuccessful()) {
-                Log.e("firebase", "Error getting data", task.getException());
-            }
-            else {
-                String membersString = String.valueOf(task.getResult().getValue());
-                Log.d("firebase", membersString);
-
-                JSONArray jsonArray = null;
-                try {
-                    jsonArray = new JSONArray(membersString);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                for(int i = 0; i < jsonArray.length(); i++) {
-                    try {
-                        members.add(jsonArray.getString(i));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
-//        int sumProgress = 0;
-//        for(String i : members) {
-//            mDatabase.child("Users").child(i).child("1").child("Reading").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-//                @Override
-//                public void onComplete(@NonNull Task<DataSnapshot> task) {
-//                    if (!task.isSuccessful()) {
-//                        Log.e("firebase", "Error getting data", task.getException());
-//                    }
-//                    else {
-//                        Log.d("firebase", String.valueOf(task.getResult().getValue()));
-//                        sumProgress += task.getResult().getValue();
-//                    }
-//                }
-//            });
-//        }
     }
 
     public void onTestClicked(View view) {
@@ -131,7 +109,7 @@ public class GroupActivity extends AppCompatActivity {
         JSONObject userContributionJSON = new JSONObject(userContribution);
         Log.d(TAG, userContributionJSON.toString());
 
-        mDatabase.child("test").setValue(userContribution);
+        mDatabase.child("progress").setValue(userContribution);
 
         Map<String, Integer> convertedUserContribution = new HashMap<>();
         JSONObject userContributionJSONObj = null;
